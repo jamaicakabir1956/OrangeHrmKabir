cda24647ac1df926edd71d050e334426c00971f5
luhan <luhan@192.168.0.10>
2020-11-28 23:20:52 -0500

    Here I change some login Steps

--- PhpTravaling1/src/test/java/com/stepDef/LoginStepsCode.java
+++ PhpTravaling1/src/test/java/com/stepDef/LoginStepsCode.java
@@ -24,7 +24,7 @@ import cucumber.api.java.en.When;
 public class LoginStepsCode {
 	WebDriver driver;
 	LoginPagePH lp; // cre-obj
-	 WebDriverWait wait;
+	WebDriverWait wait;
 
 	@Given("^user open web browser and navigate to phptravels login screen$")
 	public void user_open_web_browser_and_navigate_to_phptravels_login_screen() throws Throwable {
@@ -40,12 +40,12 @@ public class LoginStepsCode {
 
 	@Then("^user Navigate to Page and verify the Page Title Is \"([^\"]*)\"$")
 	public void user_Navigate_to_Page_and_verify_the_Page_Title_Is(String arg1) throws Throwable {
-	Thread.sleep(5000);
-	String actual = driver.getTitle();
-	System.out.println(actual);
-	String expected = "PHPTRAVELS | Travel Technology Partner";
-	Assert.assertTrue("This Tittle is Wrong", actual.contains(expected));
-	  System.out.println(expected);
+		Thread.sleep(5000);
+		String actual = driver.getTitle();
+		System.out.println(actual);
+		String expected = "PHPTRAVELS | Travel Technology Partner";
+		Assert.assertTrue("This Tittle is Wrong", actual.contains(expected));
+		System.out.println(expected);
 	}
 
 	@Then("^user enter a valid username and Password$")
@@ -66,39 +66,40 @@ public class LoginStepsCode {
 		// [@id='select2-drop']/div/input
 		login.click();
 	}
-	   
-	
 
 	@Then("^user click on the Home button$")
 	public void user_click_on_the_Home_button() throws Throwable {
 		// action.moveToElement(driver.findElement(By.xpath("//[@title ='home']"));
 		// 'home']"))).click().perform(); //[@id=\"s2id_autogen16\"]/a/span[1]
-
+		Thread.sleep(3000);
 		WebElement home = driver.findElement(By.xpath("//*[@title = 'home']"));
 		wait = new WebDriverWait(driver, 15);
 		wait.until(ExpectedConditions.elementToBeClickable(home));
 		home.click();
-	   
+
 	}
 
 	@Then("^user Select destination as Dubai, United Arab Emirates$")
 	public void user_Select_destination_as_Dubai_United_Arab_Emirates() throws Throwable {
-	    
-		//WebElement selectBox = driver.findElement(By.xpath("//*[@id=\"s2id_autogen16\"]/a/span[1]"));
-		//selectBox.click();
-		
+
+		// WebElement selectBox =
+		// driver.findElement(By.xpath("//*[@id=\"s2id_autogen16\"]/a/span[1]"));
+		// selectBox.click();
+
 		WebElement selectt = driver.findElement(By.xpath("//div[@id='s2id_autogen16']/a"));
 		selectt.click();
 		Thread.sleep(3000);
-		//WebElement select = driver.findElement(By.xpath("//*[@id='select2-drop']/div/input"));
-		//select.click();
+		// WebElement select =
+		// driver.findElement(By.xpath("//*[@id='select2-drop']/div/input"));
+		// select.click();
 		selectt.sendKeys("Dubai");
-		
+
 		WebElement dubai = driver.findElement(By.xpath("(//*[@class='select2-result-label'])[2]"));
 		wait = new WebDriverWait(driver, 15);
-	    wait.until(ExpectedConditions.visibilityOf(dubai));
-	    selectt.sendKeys(Keys.ENTER);
-	  
+		wait.until(ExpectedConditions.visibilityOf(dubai));
+		Thread.sleep(3000);
+		selectt.sendKeys(Keys.ENTER);
+
 	}
 
 	@Then("^Check-in date next month and check out next month$")
@@ -134,58 +135,60 @@ public class LoginStepsCode {
 	@Then("^user able to see all price in descending order$")
 	public void user_able_to_see_all_price_in_descending_order() throws Throwable {
 		List<WebElement> AllPrice = driver.findElements(By.xpath("//*[@class='text-secondary']"));
-		
+
 		ArrayList<String> NewList = new ArrayList<String>();
-		
-		for(int i=0; i<AllPrice.size();i++) {
+
+		for (int i = 0; i < AllPrice.size(); i++) {
 			NewList.add(AllPrice.get(i).getText().toString());
 		}
-	 // Collections.sort(NewList,Collections.reverseOrder());
-	  System.out.println("List of Price: "+NewList);
-	  String SecondPrice = NewList.get(1);
-	  System.out.println("Price of The Second Hotel: "+ SecondPrice);
-	  
+		// Collections.sort(NewList,Collections.reverseOrder());
+		System.out.println("List of Price: " + NewList);
+		String SecondPrice = NewList.get(1);
+		System.out.println("Price of The Second Hotel: " + SecondPrice);
+
 	}
 
 	@Then("^user can Select the second hotel from the list by clicking the DETAILS button$")
 	public void user_can_Select_the_second_hotel_from_the_list_by_clicking_the_DETAILS_button() throws Throwable {
-	    
+
 		JavascriptExecutor js = ((JavascriptExecutor) driver);
 		js.executeScript("window.scrollBy(0,500)");
 		WebElement details = driver.findElement(By.xpath("(//*[@class='btn btn-primary btn-sm btn-wide'])[2]"));
-		details.click(); 
+		details.click();
 	}
 
 	@Then("^user click on see price and date button on the top right and select the first room and remember the price$")
-	public void user_click_on_see_price_and_date_button_on_the_top_right_and_select_the_first_room_and_remember_the_price() throws Throwable {
-	   
+	public void user_click_on_see_price_and_date_button_on_the_top_right_and_select_the_first_room_and_remember_the_price()
+			throws Throwable {
+
 		Thread.sleep(3000);
 		WebElement seeprice = driver.findElement(By.xpath("//*[@class='anchor btn btn-primary btn-wide mt-5']"));
 		seeprice.click();
 		Thread.sleep(3000);
-		WebElement firstroom = driver.findElement(By.xpath("(//*[@class='custom-control-label text-left go-left'])[1]"));
+		WebElement firstroom = driver
+				.findElement(By.xpath("(//*[@class='custom-control-label text-left go-left'])[1]"));
 		firstroom.click();
 	}
 
 	@Then("^user Then click BOOK NOW button for checkout$")
 	public void user_Then_click_BOOK_NOW_button_for_checkout() throws Throwable {
-	    
+
 		Thread.sleep(3000);
-	    // Scroll Down using
+		// Scroll Down using
 		JavascriptExecutor js = ((JavascriptExecutor) driver);
 		js.executeScript("window.scrollBy(0,950)");
-		WebElement book = driver.findElement(By.xpath("(//*[@type='submit'])[2]"));  
+		WebElement book = driver.findElement(By.xpath("(//*[@type='submit'])[2]"));
 		book.click();
 	}
+
 	@Then("^user On the next page verify Total Amount is greater than Room Price,$")
 	public void user_On_the_next_page_verify_Total_Amount_is_greater_than_Room_Price() throws Throwable {
-	 
+
 		Thread.sleep(3000);
-		String actualstotalamount = "actualstotalamount"; 
+		String actualstotalamount = "actualstotalamount";
 		String expectedtotalamount = driver.findElement(By.xpath("//*[@class='text-main absolute-right']")).getText();
-		System.out.println("This expected price is: "+expectedtotalamount);
-		Assert.assertFalse ( actualstotalamount.equals(expectedtotalamount));
-		
+		System.out.println("This expected price is: " + expectedtotalamount);
+		Assert.assertFalse(actualstotalamount.equals(expectedtotalamount));
 
 	}
 
@@ -200,7 +203,6 @@ public class LoginStepsCode {
 
 	@Then("^user navigate to page and verify the page title is \"([^\"]*)\"$")
 	public void user_navigate_to_page_and_verify_the_page_title_is(String arg1) throws Throwable {
-	    
-	    
+
 	}
 }
